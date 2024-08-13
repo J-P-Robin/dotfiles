fd65f99d5cfe8286a53c038bc5fc35cc4642c79d
Jean-Philippe Robin <jprobin@sidlee.com>
2024-08-13 09:44:39 -0400

    update color schemes

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,11 +1,13 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	lazy = false,
-	config = function()
-		require("catppuccin").setup({
-      flavour = "mocha",
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  init = function()
+    vim.cmd.colorscheme("catppuccin")
+  end,
+  config = function()
+    require("catppuccin").setup({
+      flavour = vim.env.THEME_MODE == "light" and "latte" or "mocha",
 			transparent_background = true,
 			color_overrides = {},
 			highlight_overrides = {
@@ -14,12 +16,15 @@ return {
             --Cursor = { fg = "#ffffff", bg = colors.rosewater },
 						CursorLineNr = { fg = colors.rosewater, style = { "bold" } },
 						CursorLine = { bg = "NONE" },
+
+            CmpItemMenu = { fg = colors.surface0 },
 					}
 				end,
 			},
-		})
-	end,
-	init = function()
-		vim.cmd.colorscheme("catppuccin")
-	end,
+      integrations = {
+        mason = true,
+        neotree = true,
+      },
+    })
+  end,
 }
--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,8 +1,11 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = { 
+    'nvim-tree/nvim-web-devicons'
+  },
   config = function()
-    local catppuccin = require("catppuccin.palettes").get_palette "mocha"
+    local flavour = vim.env.THEME_MODE == "light" and "latte" or "mocha"
+    local catppuccin = require("catppuccin.palettes").get_palette(flavour)
     local custom_theme = require 'lualine.themes.catppuccin'
     custom_theme.inactive.c.bg = catppuccin.crust
 
@@ -11,6 +14,9 @@ return {
         theme = custom_theme,
         component_separators = '',
         section_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_x = {"filetype"},
       },
     })
   end,
