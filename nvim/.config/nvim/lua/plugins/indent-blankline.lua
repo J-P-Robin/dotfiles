2213daa18006086072df5acd26f312ae52be4136
Jean-Philippe Robin <jprobin@sidlee.com>
2024-08-21 15:00:26 -0400

    nvim - add IBL

---
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,17 @@
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	main = "ibl",
+	config = function()
+		require("ibl").setup({
+			indent = {
+				char = "▏",
+			},
+			scope = {
+				highlight = "IndentBlanklineIndent1",
+				show_start = false,
+				show_end = false,
+			},
+		})
+	end,
+}
