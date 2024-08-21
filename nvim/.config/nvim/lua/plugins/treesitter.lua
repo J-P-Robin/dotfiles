2c7a2091183192df0dbd7b85ee38f9e02552c76b
Jean-Philippe Robin <jprobin@sidlee.com>
2024-08-21 14:58:50 -0400

    nvim - add treesitter objects keybindings

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,46 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require "nvim-treesitter.configs".setup({
-      sync_install = true,
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"windwp/nvim-ts-autotag",
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				sync_install = true,
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-space>",
+						node_incremental = "<C-space>",
+						scope_incremental = false,
+						node_decremental = "<bs>",
+					},
+				},
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+							["as"] = "@scope",
+						},
+						selection_modes = {
+							["@parameterouter"] = "v",
+							["@function.outer"] = "V",
+							["@class.outer"] = "<C-v>",
+						},
+						include_surrounding_whitespace = true,
+					},
+				},
+			})
+		end,
+	},
 }
