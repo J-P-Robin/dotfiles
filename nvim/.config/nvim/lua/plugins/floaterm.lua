fd45d5dec84dbca002ab3ec5503b0b7fc817d623
Jean-Philippe Robin <jprobin@sidlee.com>
2024-08-21 15:11:03 -0400

    nvim - add floaterm

---
+++ nvim/.config/nvim/lua/plugins/floaterm.lua
@@ -0,0 +1,16 @@
+return {
+	"voldikss/vim-floaterm",
+	cmd = {
+		"FloatermNew",
+		"FloatermPrev",
+		"FloatermNext",
+		"FloatermFirst",
+		"FloatermLast",
+		"FloatermUpdate",
+		"FloatermToggle",
+		"FloatermShow",
+		"FloatermHide",
+		"FloatermKill",
+		"FloatermSend",
+	},
+}
